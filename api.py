c11e4d5efe4ffaab5a3956099390abbef284d7bd
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2023-02-26 16:43:32 +0300

    Add files via upload

---
+++ api.py
@@ -0,0 +1,26 @@
+import json
+import cv2
+import torch
+import numpy as np
+from fastapi import FastAPI, File
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+@app.get("/")
+async def index():
+    return {"message": "Hello YOLO World"}
+
+
+@app.post("/predict")
+async def predict(file: bytes = File(...)):
+    npimg = np.fromstring(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    results = model(img)
+    results_json = json.loads(results.pandas().xyxy[0].to_json(orient="records"))
+    return {"result" : results_json}
+
