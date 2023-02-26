5396d536f68a73b4030f5aa332ea94d949fed552
Mustafa Çavuşoğlu <65891055+mustafacavusoglu@users.noreply.github.com>
2023-02-26 16:46:07 +0300

    Add files via upload

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import requests
+
+from fastapi.testclient import TestClient
+from api import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello YOLO World"}
