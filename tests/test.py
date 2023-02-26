62ec5cf8e8594e848860476239142aca515f3553
mustafacavusoglu <mustafacavussoglu@gmail.com>
2023-02-26 17:07:20 +0300

    test added

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,7 @@ from api import app
 
 client = TestClient(app)
 
-def test_root():
+def test_index():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello YOLO World"}
